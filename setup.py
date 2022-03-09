d4afe2fcf81bae1fe0a30d8c776bd98f6d8506e6
Joachim Saul <saul@gfz-potsdam.de>
2022-03-09 07:59:23 +0100

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
+#from setuptools import setup
 from distutils.core import setup
-from distutils.extension import Extension
 
 module_name = "scdlpicker"
 
 setup(
     name='scdlpicker',
+
     version="0.1.1",
+
+    description="scdlpicker: A Python package to enable seismic phase picking based on deep-learning in SeisComP",
+
+    url='https://github.com/jsaul/scdlpicker',
+
+    author='Joachim Saul, Thomas Bornstein',
+    author_email='saul@gfz-potsdam.de',
+
+    license='AGPLv3',
+
+    keywords='SeisComP, SeisBench, ObsPy, seismology, phase picking, deep learning,',
+
+    provides=["scdlpicker"],
+
+    install_requires=['seisbench', 'obspy'],
+
+    python_requires='>=3',
+
 
     packages=['scdlpicker'],
 
